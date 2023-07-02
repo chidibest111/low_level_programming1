da69dbfc198cbe0871baf0ec6ca04a0e50477e5e
chidibest111 <rapheal4chidibest2@gmail.com>
2023-07-02 03:41:59 -0700

    task 12

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/**
+ * main - print 0 to 99
+ *
+ * Return: (0)
+ *
+ */
+int main(void)
+{
+	int i = 0;
+
+		while (i <= 99)
+		{
+			int j = 0;
+
+			while (j <= 99)
+			{
+				int firstDigit1 = i / 10;
+				int secondDigit1 = i % 10;
+				int firstDigit2 = j / 10;
+				int secondDigit2 = j % 10;
+
+				putchar(firstDigit1 + '0');
+				putchar(secondDigit1 + '0');
+				putchar(' ');
+
+				putchar(firstDigit2 + '0');
+				putchar(secondDigit2 + '0');
+				putchar(',');
+
+				j++;
+			}
+			i++;
+
+		}
+		return (0);
+}
